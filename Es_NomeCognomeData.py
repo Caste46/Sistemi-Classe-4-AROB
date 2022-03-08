48d531c684bff4fba0c78c0cf6f43deec6b34c72
Castellano Elia <91720492+Caste46@users.noreply.github.com>
2022-03-08 12:50:39 +0100

    Esercizio su dizionari

---
+++ Es_NomeCognomeData.py
@@ -0,0 +1,13 @@
+nome = (input("Inserisci un nome:"))
+cognome = (input("Inserisci un cognome:"))
+giorno = int(input("Inserisci il giorno in cui sei nato:"))
+mese = int(input("Inserisci il mese in cui sei nato:"))
+anno = int(input("Inserisci l'anno in cui sei nato:"))
+
+dizionario = {"Nome":nome, "Cognome":cognome, "Giorno":giorno, "Mese":mese, "Anno":anno}
+
+f = open("./testo2.txt", "w")
+
+for chiave in dizionario:
+    f.write(f"{dizionario[chiave]} ")
+f.close()
