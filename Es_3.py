3065ddcd881d11373d1dcc6fe53e25c81865aa99
Castellano Elia <91720492+Caste46@users.noreply.github.com>
2022-03-08 12:27:06 +0100

    Delete Es_3.py

--- Es_3.py
+++
@@ -1,6 +0,0 @@
-nome = input("Dimmi il tuo nome: ")
-anni = int(input("Dimmi la tua età: "))
-print(f"Tu ti chiami {nome} e hai {anni} anni")
-print(f"Il tuo nome inizia per {nome[0]}")
-print(f"Il tuo nome finisce per {nome[-1]}")
-print(f"La penultima lettera del tuo nome è {nome[-2]}")
