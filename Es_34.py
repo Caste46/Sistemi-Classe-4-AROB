d191a25a5ac9b17b14d93678133e973dfa9af6e9
Castellano Elia <91720492+Caste46@users.noreply.github.com>
2022-03-08 12:27:44 +0100

    Delete Es_34.py

--- Es_34.py
+++
@@ -1,7 +0,0 @@
-stringa = input("inserisci una parola: ")
-isPalindromo = lambda stringa: (stringa == stringa[::-1])
-
-if(isPalindromo == True):
-    print("E' palindroma")
-else:
-    print("NON è palindroma ")
