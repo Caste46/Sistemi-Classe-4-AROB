a8a9ac11bf980a9d2a7331814c9d37273be5f4a5
Castellano Elia <91720492+Caste46@users.noreply.github.com>
2022-03-08 12:46:33 +0100

    Lettura del file per disegnare un fiocco di neve con la Turtle

---
+++ Es_leggiFioccoDiNeve.py
@@ -0,0 +1,23 @@
+import turtle 
+snow = turtle.Turtle()
+finestra = turtle.Screen()
+
+f = open("./Istruzioni.txt", "r")
+righe = f.readlines()
+f.close()
+
+for riga in righe:
+    lista = riga.split(":")
+    
+    if(lista[0] == "forward"):
+        snow.forward(int(lista[1][:-1]))
+    elif(lista[0] == "left"):
+        snow.left(int(lista[1][:-1]))
+    elif(lista[0] == "right"):
+        snow.right(int(lista[1][:-1]))
+    elif(lista[0] == "goto"):
+        snow.goto(0,0)
+    elif(lista[0] == "speed"):
+        snow.speed(int(lista[1][:-1]))
+
+finestra.exitonclick()
