4379b1f6685ef3461c63767210b75cbd80c80cad
Castellano Elia <91720492+Caste46@users.noreply.github.com>
2022-03-08 12:27:38 +0100

    Delete Es_33.py

--- Es_33.py
+++
@@ -1,7 +0,0 @@
-frase = input("inserisci una  frase : ")
-controllo = lambda frase: (frase.isupper())
-ok = controllo(frase[0])
-if(ok == True):
-    print("Inizia con la lettera maiuscola ")
-else:
-    print("Non inizia con la lettera maiuscola ")
