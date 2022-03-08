f4aa877d0e01db2647825a556f076a816dbe5996
Castellano Elia <91720492+Caste46@users.noreply.github.com>
2022-03-08 12:25:02 +0100

    Delete Es_16.py

--- Es_16.py
+++
@@ -1,7 +0,0 @@
-rubrica = {"ale":380435, "matti":120786}
-nome = input("Inserisci il nome: ")
-print(rubrica[nome])
-nuovo_amico = input("Inserisci il nome da aggiungere: ")
-nuovo_telefono = input("Inserisci il numero: ")
-rubrica[nuovo_amico] = nuovo_telefono
-print(rubrica)
