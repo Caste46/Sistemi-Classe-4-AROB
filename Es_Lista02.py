d25e0663cd6980032d08558f770ad2682df1f10c
Castellano Elia <91720492+Caste46@users.noreply.github.com>
2022-03-08 12:48:38 +0100

    Es 2 Liste

---
+++ Es_Lista02.py
@@ -0,0 +1,12 @@
+#1) chiedere stringa utente
+#2) ciclo sulla stringa in cui snobbo tutto ciò che non è 
+#      ()[]{}
+#3) se trovo parentesi aperta faccio push
+#4) se trovo parentesi chiusa 
+
+stringa = "abc()"
+lista = []
+
+for k in stringa:
+    if stringa[k] == "(" or "[" or "{":
+        lista.append = 
